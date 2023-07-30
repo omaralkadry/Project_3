d0fe62e3677dcfd13155b3f848bc3fe9928b43dd
omaralkadry <omar.alkadry@ufl.edu>
2023-07-29 22:32:25 -0400

    offender adjusted to contain multiple people who all live at the same zipcode

--- Offender.cpp
+++ Offender.cpp
@@ -1,37 +1,48 @@
 //
-// Created by omara on 7/29/2023.
+// Created by omar on 7/29/2023.
 //
 using namespace std;
 
 class Offender {
-public:
-    string name;
-    string address;
+    vector<string> names;
+    vector<string> addresses;
     int zipcode;
-    Offender(string name, string address, int zipcode);
-    string Get_name() const;
-    string Get_address() const;
+public:
+    Offender();
+    Offender(int zipcode);
+    vector<string> Get_names() const;
+    vector<string> Get_addresses() const;
     int Get_zipcode() const;
+    void Add_offender(const string& name, const string& address);
 };
 
-Offender::Offender(string name, string address, int zipcode)
+Offender::Offender()
 {
-    this->name = name;
-    this->address = address;
+    this->zipcode = -1;
+}
+
+Offender::Offender(int zipcode)
+{
     this->zipcode = zipcode;
 }
 
-string Offender::Get_name() const
+vector<string> Offender::Get_names() const
 {
-    return this->name;
+    return this->names;
 }
 
-string Offender::Get_address() const
+vector<string> Offender::Get_addresses() const
 {
-    return this->address;
+    return this->addresses;
 }
 
 int Offender::Get_zipcode() const
 {
     return this->zipcode;
+}
+
+void Offender::Add_offender(const string &name, const string &address)
+{
+    this->names.push_back(name);
+    this->addresses.push_back(address);
 }
