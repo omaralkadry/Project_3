0dbbf2c6ca1ce368d040bd0ac762a65465c32484
seanlewertow <116752300+seanlewertow@users.noreply.github.com>
2023-08-02 17:43:37 -0400

    added comments

--- Hash_table.h
+++ Hash_table.h
@@ -1,18 +1,25 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 #include "Person.cpp"
+
 using namespace std;
 
-class HashTable{
+class HashTable {
 private:
-    vector<Person> table;
-    int numPeople;
+    vector<Person> table; // vector of Person objects
+    int numPeople; // num people in the hash table
+
     HashTable();
-    int Hash(int zipcode);
+    int Hash(int zipcode); // hash function to map zipcodes to table indices
+
 public:
+    // Search for a person by their zipcode
+    // Returns the Person object if found
     Person search(int zipcode);
+
+    // Insert a new person into the hash table
     void insert(Person& person);
+
     void print();
 };
-
