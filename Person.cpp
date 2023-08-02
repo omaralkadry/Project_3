9aee3cae23354683380c89720af9e8792e49ae97
seanlewertow <116752300+seanlewertow@users.noreply.github.com>
2023-08-02 17:34:54 -0400

    cleaned up header and added comments

--- Person.cpp
+++ Person.cpp
@@ -1,66 +1,65 @@
 //
 // Created by omar on 7/29/2023.
 //
-#include<iostream>
-#include<vector>
-#include<string>
-#include<sstream>
-#include<fstream>
-#include"Person.h"
-using namespace std;
+#include "Person.h"
 
 Person::Person()
 {
+    // Initialize zipcode to a default value of -1, indicating no zipcode provided.
     this->zipcode = -1;
 }
 
 Person::Person(int zipcode)
 {
+    // sets the provided zipcode when creating a new Person object.
     this->zipcode = zipcode;
 }
 
-vector<string> Person::Get_names() 
+vector<string> Person::Get_names()
 {
+    // Return the vector of names associated with this Person.
     return this->names;
 }
 
-vector<string> Person::Get_addresses() 
+vector<string> Person::Get_addresses()
 {
+    // Return the vector of addresses associated with this Person.
     return this->addresses;
 }
 
-int Person::Get_zipcode() 
+int Person::Get_zipcode()
 {
+    // Return the zipcode of this Person.
     return this->zipcode;
 }
 
-void Person::Add_offender(string &name, string &address)
+void Person::Add_offender(string& name, string& address)
 {
+    // appending their name and address to the respective vectors.
     this->names.push_back(name);
     this->addresses.push_back(address);
 }
 
-void Person::Deserialize(istringstream& stream){
+void Person::Deserialize(istringstream& stream)
+{
     string token;
 
-	getline(stream, this->name, ',');
-
-	getline(stream, this->address, ',');
-
+    // Read name, address, city, state, zipcode, and county from the stream
+    getline(stream, this->name, ',');
+    getline(stream, this->address, ',');
     getline(stream, this->city, ',');
-
     getline(stream, this->state, ',');
-
-	getline(stream, token, ',');
-	this->zipcode = stoi(token);
-
+    getline(stream, token, ',');
+    this->zipcode = stoi(token);
     getline(stream, this->county, ',');
 }
 
-void Person::PrintInfo(){
+void Person::PrintInfo()
+{
+    // Print the information of this Person
     cout << "Name: " << this->name << endl;
-	cout << "Address: " << this->address << endl;
-	cout << "City: " << this->city << endl;
-	cout << "State:" << this->state << endl;
-	cout << "Zipcode: " << this->zipcode << endl;
+    cout << "Address: " << this->address << endl;
+    cout << "City: " << this->city << endl;
+    cout << "State:" << this->state << endl;
+    cout << "Zipcode: " << this->zipcode << endl;
 }
