06f2580353f3edb5dc7c2b30576ec75b228dd7e9
seanlewertow <116752300+seanlewertow@users.noreply.github.com>
2023-08-02 17:09:51 -0400

    Create Bplus_tree.h
    definitions for b+ tree implementation

---
+++ Bplus_tree.h
@@ -0,0 +1,31 @@
+#ifndef BPLUSTREE_H
+#define BPLUSTREE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "Person.h"
+
+class Node {
+public:
+    bool is_leaf;
+    std::vector<int> zipcodes;
+    std::vector<Person> data;
+    std::vector<Node*> children;
+    explicit Node(bool leaf_bool);
+};
+
+class BPlusTree {
+public:
+    BPlusTree();
+    Node* Get_root(); // unused
+    void insert(int zipcode, const std::string& name, const std::string& address);
+    void search(int zipcode);
+
+private:
+    Node* root;
+    const int order = 100; // adjust order value according to the size of total data
+};
+
+#endif // BPLUSTREE_H
