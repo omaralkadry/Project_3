43eee5015698c8b22eff89bd2f3f32ce4204401d
Elli Ludwin <e.ludwin@ufl.edu>
2023-08-02 13:47:46 -0400

    Added data files and modified main, Person, and started on Hashtable

--- Hash_table.cpp
+++ Hash_table.cpp
@@ -1,3 +1,18 @@
-//
-// Created by omara on 7/29/2023.
-//
+#include<iostream>
+#include<string>
+#include<vector>
+#include "Person.cpp"
+using namespace std;
+
+class HashTable{
+private:
+    vector<Person> table;
+    int numPeople;
+    HashTable();
+    int Hash(int zipcode);
+public:
+    Person search(int zipcode);
+    void insert(Person& person);
+    void print();
+};
+
---
+++ Person.cpp
@@ -0,0 +1,66 @@
+//
+// Created by omar on 7/29/2023.
+//
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include"Person.h"
+using namespace std;
+
+Person::Person()
+{
+    this->zipcode = -1;
+}
+
+Person::Person(int zipcode)
+{
+    this->zipcode = zipcode;
+}
+
+vector<string> Person::Get_names() 
+{
+    return this->names;
+}
+
+vector<string> Person::Get_addresses() 
+{
+    return this->addresses;
+}
+
+int Person::Get_zipcode() 
+{
+    return this->zipcode;
+}
+
+void Person::Add_offender(string &name, string &address)
+{
+    this->names.push_back(name);
+    this->addresses.push_back(address);
+}
+
+void Person::Deserialize(istringstream& stream){
+    string token;
+
+	getline(stream, this->name, ',');
+
+	getline(stream, this->address, ',');
+
+    getline(stream, this->city, ',');
+
+    getline(stream, this->state, ',');
+
+	getline(stream, token, ',');
+	this->zipcode = stoi(token);
+
+    getline(stream, this->county, ',');
+}
+
+void Person::PrintInfo(){
+    cout << "Name: " << this->name << endl;
+	cout << "Address: " << this->address << endl;
+	cout << "City: " << this->city << endl;
+	cout << "State:" << this->state << endl;
+	cout << "Zipcode: " << this->zipcode << endl;
+}
