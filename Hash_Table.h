23a84d4d538ad60324d1b5d6fd28a4eb9db22e82
Elli Ludwin <e.ludwin@ufl.edu>
2023-08-03 01:06:24 -0400

    Added hash function etc

--- Hash_Table.h
+++ Hash_Table.h
@@ -6,6 +6,8 @@ using namespace std;
 
 class HashTable {
 private:
+    HashTable();
+    ~HashTable();
     static const int capacity = 100;
     static const double loadfact;
 
@@ -17,9 +19,51 @@ private:
     double loadFactor() const;
 
 public:
-    HashTable();
-    ~HashTable();
-
-    void insert(const Person& person);
+    void insert (Person& person);
     vector<Person> searchByZipcode(int zipcode) const;
 };
+
+/*=== CONSTRUCTOR/DESTRUCTOR===*/
+HashTable::HashTable(){
+    size = 0;
+    table.resize(capacity);
+}
+
+HashTable::~HashTable(){
+    for(auto& bucket : table){
+        bucket.clear();
+    }
+}
+
+int HashTable::hash(int key) const {
+    return key % capacity; // this does not resolve collision cases when/if zipcode values are duplicate
+}
+
+void HashTable::rehash(){
+    // Double the capacity and create a new empty table with the new capacity
+    vector<list<pair<int, Person>>> newTable;
+    newTable = table;
+    table.resize(newTable.size() * 2);
+
+    // Rehash all elements from the old table to the new table
+    for (const auto& bucket : table) {
+        for (const auto& data : bucket) {
+            int key = hash(data.first);
+            newTable[key].push_back(data);
+        }
+    }
+}
+
+void HashTable::insert(Person& person){
+    int hashVal = hash(person.Get_zipcode());
+    table[hashVal].push_back(make_pair(person.Get_zipcode(), person));
+    size++;
+
+    if(loadFactor() > loadfact){
+        rehash();
+    }
+}
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -2,37 +2,35 @@
 #include <fstream>
 #include <sstream>
 #include "Bplus_tree.cpp"
-#include "Hash_table.cpp"
-#include "Person.cpp"
+#include "Hash_Table.h"
 
 using namespace std;
 
-
-
 int main() {
-    vector<Person> people;
-    void LoadData(vector<Person>& people);
-    LoadData(people);
-    for(int i = 0; i < people.size(); i++){
-        people[i].PrintInfo();
-    }
+    // vector<Person> people;
+    // void LoadData(vector<Person>& people);
+    // LoadData(people);
+    // for(int i = 0; i < people.size(); i++){
+    //     people[i].PrintInfo();
+    // }
 
     return 0;
 }
 
-void LoadData(vector<Person>& output){
-    ifstream csv("temp_data.csv");
-        if (csv.is_open()) {
-            string line;
-            getline(csv, line);// Get rid of the header line.
-            while (getline(csv, line)) {
-                istringstream stream(line);
-                Person person;
-                person.Deserialize(stream);
-                output.push_back(person);
-            }
-        }
-        else {
-            cout << "Error! File couldn't open.";
-        }
-}
+// void HashTable::LoadData(vector<Person>& output){
+//     ifstream csv("temp_data.csv");
+//         if (csv.is_open()) {
+//             string line;
+//             getline(csv, line);// Get rid of the header line.
+//             while (getline(csv, line)) {
+//                 istringstream stream(line);
+//                 Person person;
+//                 person.Deserialize(stream);
+//                 output.push_back(person);
+//             }
+//         }
+//         else {
+//             cout << "Error! File couldn't open.";
+//         }
+// }
+
