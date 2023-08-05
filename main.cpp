fcc7e88ecd4e530b0fff5938addc5663906823e5
seanlewertow <116752300+seanlewertow@users.noreply.github.com>
2023-08-05 15:32:59 -0400

    added valid input function and menu option 3

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ using namespace std;
 
 void LoadData(string& file, BPlusTree& tree, HashTable& table);
 
+// Function to validate and get an integer input
+int getValidIntegerInput(const string& message);
+
 int main() {
 
     string filename = "data.csv";
@@ -19,31 +22,17 @@ int main() {
 
     string input = "0";
 
-    while (input != "3") {
+    while (input != "4") {
         cout << "Menu:" << endl;
         cout << "1. Search by zipcode" << endl;
         cout << "2. Add person" << endl;
-        cout << "3. Exit" << endl;
-        cout << "Enter selection: (1,2 or 3)" << endl;
+        cout << "3. Remove person" << endl;
+        cout << "4. Exit program" << endl;
+        cout << "Enter selection: (1,2, 3, or 4)" << endl;
         cin >> input;
 
         if (input == "1") {
-            int zipcode;
-            bool isValidInput = false;
-
-            while (!isValidInput) {
-                cout << "Enter zipcode to search for: " << endl;
-                cin >> zipcode;
-                cout << endl;
-
-                if (cin.fail()) { // only accept numeric input
-                    cin.clear();
-                    cin.ignore();
-                    cout << "Invalid Input: Zipcode must be numeric." << endl;
-                } else {
-                    isValidInput = true; // Input is valid, exit loop
-                }
-            }
+            int zipcode = getValidIntegerInput("Enter zipcode to search for:");
 
             cout << "B+ Tree's output: " << endl;
             auto start = chrono::high_resolution_clock::now();
@@ -77,22 +66,7 @@ int main() {
             cout << "Enter Person's Address or any information regarding their location: " << endl;
             getline(cin,address);
 
-            int zipcode;
-            bool isValidInput = false;
-
-            while (!isValidInput) {
-                cout << "Enter Person's Zipcode or 0 if address is not available: " << endl;
-                cin >> zipcode;
-                cout << endl;
-
-                if (cin.fail()) { // only accept numeric input
-                    cin.clear();
-                    cin.ignore();
-                    cout << "Invalid Input: Zipcode must be numeric." << endl;
-                } else {
-                    isValidInput = true; // Input is valid, exit loop
-                }
-            }
+            int zipcode = getValidIntegerInput("Enter Person's Zipcode or 0 if address is not available:");
 
             // Insertion into B+ Tree with time-lapse
             auto start = chrono::high_resolution_clock::now();
@@ -115,6 +89,38 @@ int main() {
             cout << endl;
         }
         else if (input == "3") {
+            string name;
+            cin.ignore();
+            cout << "Enter Person's Name to remove: " << endl;
+            getline(cin, name);
+
+            string address;
+            cout << "Enter Person's Address to remove: " << endl;
+            getline(cin, address);
+
+            int zipcode = getValidIntegerInput("Enter Person's Zipcode to remove:");
+
+            // Removing from B+ Tree with time-lapse
+            auto start = chrono::high_resolution_clock::now();
+            Tree.change_address(zipcode, name, address);
+            auto stop = chrono::high_resolution_clock::now();
+            auto time_lapse = chrono::duration_cast<chrono::microseconds>(stop - start);
+            cout << endl;
+            cout << "Time lapsed for Removing in B+ Tree is: " << time_lapse.count() << " microseconds" << endl;
+            cout << endl;
+            cout << endl;
+
+            // Removing from Hash Table with time-lapse
+            start = chrono::high_resolution_clock::now();
+            Table.remove(name, zipcode, address);
+            stop = chrono::high_resolution_clock::now();
+            time_lapse = chrono::duration_cast<chrono::microseconds>(stop - start);
+            cout << endl;
+            cout << "Time lapsed for Removing in Hash Table is: " << time_lapse.count() << " microseconds" << endl;
+            cout << endl;
+            cout << endl;
+        }
+        else if (input == "4") {
             cout << "Thank you for using the Meghan's Law Program." << endl;
         }
         else {
@@ -168,4 +174,25 @@ void LoadData(string& file, BPlusTree& tree, HashTable& table)
     else {
         cout << "Error! File couldn't open.";
     }
+}
+
+int getValidIntegerInput(const string& message) {
+    int value;
+    bool isValidInput = false;
+
+    while (!isValidInput) {
+        cout << message << endl;
+        cin >> value;
+        cout << endl;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Invalid Input: Please enter a valid integer." << endl;
+        } else {
+            isValidInput = true;
+        }
+    }
+
+    return value;
 }
